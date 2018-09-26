386082dcc368870c82fcd4af127fc9c92782ccd7
Monalisa98927 <380923800@qq.com>
2018-09-26 21:04:14 +0800

    first commit

---
+++ src/mockData/mockMessages.ts
@@ -0,0 +1,46 @@
+export var mockMessage = [
+    {
+      "id": '001',
+      "date": '1532943051',
+      "userId": '123',
+      "username": 'aaa',
+      "pic": 'url',
+      "text": 'Tomorrow we will go shopping,would you like to join us?'
+    },
+    {
+        "id": '002',
+        "date": '1532943051',
+        "userId": '234',
+        "username": 'aaa',
+        "pic": 'url',
+        "text": 'Really...That sounds great!'
+    },{
+        "id": '003',
+        "date": '1532943051',
+        "userId": '345',
+        "username": 'aaa',
+        "pic": 'url',
+        "text": 'Tomorrow we will go shopping,would you like to join us?'
+      },{
+        "id": '004',
+        "date": '1532943051',
+        "userId": '456',
+        "username": 'aaa',
+        "pic": 'url',
+        "text": 'Really...That sounds great!'
+    },{
+        "id": '005',
+        "date": '1532943051',
+        "userId": '567',
+        "username": 'aaa',
+        "pic": 'url',
+        "text": 'go shopping!!'
+      },{
+        "id": '006',
+        "date": '1534949068',
+        "userId": '678',
+        "username": 'aaa',
+        "pic": 'url',
+        "text": 'hhahahahaha'
+      }
+  ];
